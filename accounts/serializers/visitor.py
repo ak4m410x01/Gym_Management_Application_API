b0ebc2a36fb75de85f3f5274fa9ff25ea58790b6
ak4m410x01 <abdallah10kamal@gmail.com>
2024-01-27 15:49:06 +0200

    implement create & delete on visitor models

--- accounts/serializers/visitor.py
+++ accounts/serializers/visitor.py
@@ -1,26 +1,32 @@
+from re import match
 from rest_framework import serializers
 from accounts.models.visitor import Visitor
+from accounts.models.account import Account, Contact
 
 
 class VisitorSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(source="account.email")
     username = serializers.CharField(source="account.username")
+    password = serializers.CharField(source="account.password")
     first_name = serializers.CharField(source="account.first_name")
     last_name = serializers.CharField(source="account.last_name")
     gender = serializers.CharField(source="account.gender")
     date_of_birth = serializers.DateField(source="account.date_of_birth")
-    country = serializers.CharField(source="account.country")
-    city = serializers.CharField(source="account.city")
-    address = serializers.CharField(source="account.address")
-    first_login = serializers.DateTimeField(source="account.first_login")
-    last_login = serializers.DateTimeField(source="account.last_login")
-    joined_at = serializers.DateTimeField(source="account.joined_at")
-    phone = serializers.CharField(source="account.contact.phone")
-    whatsapp = serializers.CharField(source="account.contact.whatsapp")
-    telegram = serializers.CharField(source="account.contact.telegram")
-    facebook = serializers.CharField(source="account.contact.facebook")
-    instagram = serializers.CharField(source="account.contact.instagram")
-    twitter = serializers.CharField(source="account.contact.twitter")
+    city = serializers.CharField(source="account.city", required=False)
+    address = serializers.CharField(source="account.address", required=False)
+    phone = serializers.CharField(source="account.contact.phone", required=False)
+    whatsapp = serializers.CharField(source="account.contact.whatsapp", required=False)
+    telegram = serializers.CharField(source="account.contact.telegram", required=False)
+    facebook = serializers.CharField(source="account.contact.facebook", required=False)
+    instagram = serializers.CharField(
+        source="account.contact.instagram", required=False
+    )
+    twitter = serializers.CharField(source="account.contact.twitter", required=False)
+    first_login = serializers.DateTimeField(
+        source="account.first_login", required=False
+    )
+    last_login = serializers.DateTimeField(source="account.last_login", required=False)
+    joined_at = serializers.DateTimeField(source="account.joined_at", required=False)
 
     class Meta:
         model = Visitor
@@ -28,11 +34,11 @@ class VisitorSerializer(serializers.ModelSerializer):
             "id",
             "email",
             "username",
+            "password",
             "first_name",
             "last_name",
             "gender",
             "date_of_birth",
-            "country",
             "city",
             "address",
             "phone",
@@ -45,3 +51,29 @@ class VisitorSerializer(serializers.ModelSerializer):
             "last_login",
             "joined_at",
         ]
+
+    def validate_username(self, username: str) -> str:
+        if Account.objects.filter(username=username).exists():
+            raise serializers.ValidationError(f"username already exists.")
+        return username
+
+    def validate_email(self, email: str) -> str:
+        if Account.objects.filter(email=email).exists():
+            raise serializers.ValidationError(f"email already exists.")
+        return email
+
+    def validate_gender(self, gender: str) -> str:
+        regex = r"^[MF]$"
+        if not match(regex, gender):
+            raise serializers.ValidationError(f"gender must be 'M' or 'F'.")
+        return gender
+
+    def create(self, validated_data):
+        contact_data = validated_data.pop("contact", {})
+        account_data = validated_data.pop("account", {})
+
+        contact = Contact.objects.create(**contact_data)
+        account = Account.objects.create(contact=contact, **account_data)
+        visitor = Visitor.objects.create(account=account, **validated_data)
+
+        return visitor
--- accounts/views/visitor.py
+++ accounts/views/visitor.py
@@ -1,5 +1,8 @@
-from rest_framework import generics
+from django.db import IntegrityError
+from rest_framework import generics, status
+from rest_framework.response import Response
 from accounts.models.visitor import Visitor
+from accounts.models.account import Account, Contact
 from accounts.serializers.visitor import VisitorSerializer
 
 
@@ -7,7 +10,15 @@ class VisitorListCreate(generics.ListCreateAPIView):
     queryset = Visitor.objects.all()
     serializer_class = VisitorSerializer
 
-
 class VisitorRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     queryset = Visitor.objects.all()
     serializer_class = VisitorSerializer
+
+    def perform_destroy(self, instance):
+        account = Account.objects.get(id=instance.account.id)
+        contact = Contact.objects.get(id=account.contact.id)
+
+        contact.delete()
+        account.delete()
+
+        return super().perform_destroy(instance)
