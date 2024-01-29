60f655b79df8f87999f04a014dda49993f902393
ak4m410x01 <abdallah10kamal@gmail.com>
2024-01-29 17:28:28 +0200

    update page size to 20 object per page

--- project/settings.py
+++ project/settings.py
@@ -92,7 +92,7 @@ ROOT_URLCONF = "project.urls"
 
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
-    "PAGE_SIZE": 100,
+    "PAGE_SIZE": 20,
 }
 
 TEMPLATES = [
