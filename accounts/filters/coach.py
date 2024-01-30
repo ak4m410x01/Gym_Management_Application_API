44178f4b8d6e12d2dc8d424a53dfb89f99ed142a
ak4m410x01 <abdallah10kamal@gmail.com>
2024-01-30 04:24:20 +0200

    fix: Cannot resolve keyword 'username' into field

--- accounts/filters/coach.py
+++ accounts/filters/coach.py
@@ -3,10 +3,25 @@ from accounts.models.coach import Coach
 
 
 class CoachFilter(django_filters.FilterSet):
-    username = django_filters.CharFilter(lookup_expr="icontains")
-    email = django_filters.CharFilter(lookup_expr="icontains")
-    first_name = django_filters.CharFilter(lookup_expr="icontains")
-    last_name = django_filters.CharFilter(lookup_expr="icontains")
+    username = django_filters.CharFilter(
+        field_name="account__username",
+        lookup_expr="icontains",
+    )
+
+    email = django_filters.CharFilter(
+        field_name="account__username",
+        lookup_expr="icontains",
+    )
+
+    first_name = django_filters.CharFilter(
+        field_name="account__username",
+        lookup_expr="icontains",
+    )
+
+    last_name = django_filters.CharFilter(
+        field_name="account__username",
+        lookup_expr="icontains",
+    )
 
     class Meta:
         model = Coach
--- accounts/filters/member.py
+++ accounts/filters/member.py
@@ -3,10 +3,26 @@ from accounts.models.member import Member
 
 
 class MemberFilter(django_filters.FilterSet):
-    username = django_filters.CharFilter(lookup_expr="icontains")
-    email = django_filters.CharFilter(lookup_expr="icontains")
-    first_name = django_filters.CharFilter(lookup_expr="icontains")
-    last_name = django_filters.CharFilter(lookup_expr="icontains")
+    username = django_filters.CharFilter(
+        field_name="account__username",
+        lookup_expr="icontains",
+    )
+
+    email = django_filters.CharFilter(
+        field_name="account__username",
+        lookup_expr="icontains",
+    )
+
+    first_name = django_filters.CharFilter(
+        field_name="account__username",
+        lookup_expr="icontains",
+    )
+
+    last_name = django_filters.CharFilter(
+        field_name="account__username",
+        lookup_expr="icontains",
+    )
+
 
     class Meta:
         model = Member
