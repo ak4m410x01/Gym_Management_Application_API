816f13d4b3910364cd96e69fe73a4424dd712ea9
ak4m410x01 <abdallah10kamal@gmail.com>
2024-01-31 23:26:13 +0200

    change access & refresh token lifetime

--- project/settings.py
+++ project/settings.py
@@ -88,8 +88,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=180),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=50),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=7),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=14),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
     "UPDATE_LAST_LOGIN": False,
