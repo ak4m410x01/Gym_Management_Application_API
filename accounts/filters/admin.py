6982642cd4070ebdaf470606eed96cc2b50cb9b8
ak4m410x01 <abdallah10kamal@gmail.com>
2024-02-01 04:50:09 +0200

    fix: lookup_fields name

--- accounts/filters/admin.py
+++ accounts/filters/admin.py
@@ -4,9 +4,9 @@ from accounts.models.admin import Admin
 
 class AdminFilter(django_filters.FilterSet):
     username = django_filters.CharFilter(field_name="user__username", lookup_expr="icontains")
-    email = django_filters.CharFilter(field_name="user__username", lookup_expr="icontains")
-    first_name = django_filters.CharFilter(field_name="user__username", lookup_expr="icontains")
-    last_name = django_filters.CharFilter(field_name="user__username", lookup_expr="icontains")
+    email = django_filters.CharFilter(field_name="user__email", lookup_expr="icontains")
+    first_name = django_filters.CharFilter(field_name="user__first_name", lookup_expr="icontains")
+    last_name = django_filters.CharFilter(field_name="user__last_name", lookup_expr="icontains")
 
     class Meta:
         model = Admin
