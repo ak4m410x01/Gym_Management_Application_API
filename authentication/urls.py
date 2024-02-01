980732d633ecf8da0f97e940d1feff7020e36c3c
ak4m410x01 <abdallah10kamal@gmail.com>
2024-02-01 02:35:58 +0200

    add jwt tokens

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenVerifyView
+
+
 from authentication.views.signup import SignUp
 from authentication.views.verifyemail import VerifyEmail
 from authentication.views.signin import SignIn
@@ -9,4 +13,8 @@ urlpatterns = [
     path("signup/", SignUp.as_view(), name="signup"),
     path("signup/verify/", VerifyEmail.as_view(), name="verifyemail"),
     path("signin/", SignIn.as_view(), name="signin"),
+    # Token
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
 ]
