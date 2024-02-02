454aa075cd2c62679deea6692e7be2b2a3547abe
ak4m410x01 <abdallah10kamal@gmail.com>
2024-02-02 03:55:43 +0200

    remove create view for member

--- accounts/views/member.py
+++ accounts/views/member.py
@@ -1,15 +1,18 @@
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.generics import ListAPIView, RetrieveUpdateDestroyAPIView
 from django_filters.rest_framework import DjangoFilterBackend
 
 from accounts.models.user import User, Contact
 from accounts.models.member import Member
 from accounts.serializers.member import MemberSerializer
 from accounts.filters.member import MemberFilter
+from accounts.permissions.isAdmin import IsAdmin
+from accounts.permissions.isMember import IsMember
 
 
-class MemberListCreate(ListCreateAPIView):
+class MemberList(ListAPIView):
     queryset = Member.objects.all()
     serializer_class = MemberSerializer
+    permission_classes = (IsAdmin)
 
 
 class MemberRetrieveUpdateDestroy(RetrieveUpdateDestroyAPIView):
@@ -17,6 +20,7 @@ class MemberRetrieveUpdateDestroy(RetrieveUpdateDestroyAPIView):
     serializer_class = MemberSerializer
     filter_backends = (DjangoFilterBackend,)
     filterset_class = MemberFilter
+    permission_classes = (IsMember,)
 
     def perform_destroy(self, instance):
         user = User.objects.get(id=instance.user.id)
