e67538800e64894c67d4c76267c944e784218fad
ak4m410x01 <abdallah10kamal@gmail.com>
2024-02-02 04:09:37 +0200

    fix: import member error

--- accounts/urls/member.py
+++ accounts/urls/member.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from accounts.views.member import MemberListCreate, MemberRetrieveUpdateDestroy
+from accounts.views.member import MemberList, MemberRetrieveUpdateDestroy
 
 urlpatterns = [
-    path("", MemberListCreate.as_view()),
+    path("", MemberList.as_view()),
     path("<int:pk>/", MemberRetrieveUpdateDestroy.as_view()),
 ]
