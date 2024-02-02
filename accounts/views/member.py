911be8f03297a815aa7005613c69e0571ba971ec
ak4m410x01 <abdallah10kamal@gmail.com>
2024-02-02 05:24:33 +0200

    set any user can retrieve info about member

--- accounts/views/member.py
+++ accounts/views/member.py
@@ -1,25 +1,24 @@
 from rest_framework.generics import ListAPIView, RetrieveUpdateDestroyAPIView
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.permissions import IsAuthenticated
 
 from accounts.models.user import User, Contact
 from accounts.models.member import Member
 from accounts.serializers.member import MemberSerializer
 from accounts.filters.member import MemberFilter
-from accounts.permissions.isAdmin import IsAdmin
 from accounts.permissions.isMember import IsMember
 
 
 class MemberList(ListAPIView):
     queryset = Member.objects.all()
     serializer_class = MemberSerializer
-    permission_classes = (IsAdmin)
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = MemberFilter
 
 
 class MemberRetrieveUpdateDestroy(RetrieveUpdateDestroyAPIView):
     queryset = Member.objects.all()
     serializer_class = MemberSerializer
-    filter_backends = (DjangoFilterBackend,)
-    filterset_class = MemberFilter
     permission_classes = (IsMember,)
 
     def perform_destroy(self, instance):
@@ -30,3 +29,8 @@ class MemberRetrieveUpdateDestroy(RetrieveUpdateDestroyAPIView):
         user.delete()
 
         return super().perform_destroy(instance)
+
+    def get_permissions(self):
+        if self.request.method == "GET":
+            return (IsAuthenticated,)
+        return super().get_permissions()
