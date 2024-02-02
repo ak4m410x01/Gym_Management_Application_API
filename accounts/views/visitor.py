eaf540051d9b4d3b8feb6ee3dd78e8bfffbd43f5
ak4m410x01 <abdallah10kamal@gmail.com>
2024-02-02 05:23:45 +0200

    allow filter in visitor list view

--- accounts/views/visitor.py
+++ accounts/views/visitor.py
@@ -1,15 +1,19 @@
 from rest_framework.generics import ListAPIView, RetrieveUpdateDestroyAPIView
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.permissions import IsAuthenticated
 
 from accounts.models.visitor import Visitor
 from accounts.models.user import User, Contact
 from accounts.serializers.visitor import VisitorSerializer
+from accounts.filters.visitor import VisitorFilter
 from accounts.permissions.isVisitor import IsVisitor
 
 
 class VisitorList(ListAPIView):
     queryset = Visitor.objects.all()
     serializer_class = VisitorSerializer
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = VisitorFilter
 
 
 class VisitorRetrieveUpdateDestroy(RetrieveUpdateDestroyAPIView):
