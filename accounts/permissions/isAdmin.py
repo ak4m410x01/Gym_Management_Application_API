038db46c696e74753904487c7afc7acb8e3a3653
ak4m410x01 <abdallah10kamal@gmail.com>
2024-02-05 14:14:09 +0200

    recustomize permissions

--- accounts/permissions/isAdmin.py
+++ accounts/permissions/isAdmin.py
@@ -1,28 +1,14 @@
-from jwt import decode
-from decouple import config
-
 from rest_framework.permissions import BasePermission
+from authentication.utils.token import JWTToken
 
 
 class IsAdmin(BasePermission):
     def has_permission(self, request, view):
         token = request.auth.token.decode()
-
-        payload = decode(
-            token,
-            key=config("JWT_SECRET_KEY"),
-            algorithms=["HS256"],
-        )
-
-        return payload.get("role") == "admin"
+        payload = JWTToken.get_payload(token)
+        return payload.get("user_role") == "admin"
 
     def has_object_permission(self, request, view, obj):
         token = request.auth.token.decode()
-
-        payload = decode(
-            token,
-            key=config("JWT_SECRET_KEY"),
-            algorithms=["HS256"],
-        )
-
+        payload = JWTToken.get_payload(token)
         return payload.get("username") == obj.user.username
--- accounts/permissions/isMember.py
+++ accounts/permissions/isMember.py
@@ -1,29 +1,14 @@
-from jwt import decode
-from decouple import config
-
 from rest_framework.permissions import BasePermission
+from authentication.utils.token import JWTToken
 
 
 class IsMember(BasePermission):
     def has_permission(self, request, view):
         token = request.auth.token.decode()
+        payload = JWTToken.get_payload(token)
+        return payload.get("user_role") == "member"
 
-        payload = decode(
-            token,
-            key=config("JWT_SECRET_KEY"),
-            algorithms=["HS256"],
-        )
-
-        return payload.get("role") == "member"
-    
     def has_object_permission(self, request, view, obj):
         token = request.auth.token.decode()
-
-        payload = decode(
-            token,
-            key=config("JWT_SECRET_KEY"),
-            algorithms=["HS256"],
-        )
-
+        payload = JWTToken.get_payload(token)
         return payload.get("username") == obj.user.username
-
