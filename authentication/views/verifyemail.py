1f0a06d2fd76167d9860d3eb4fe3a562aeacaf80
ak4m410x01 <abdallah10kamal@gmail.com>
2024-02-06 22:58:20 +0200

    remove unused imports

--- authentication/views/verifyemail.py
+++ authentication/views/verifyemail.py
@@ -1,5 +1,4 @@
 import jwt
-from decouple import config
 from rest_framework.generics import GenericAPIView
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
